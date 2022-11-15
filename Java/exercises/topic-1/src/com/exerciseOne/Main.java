57a75ef9c3f099cf1df10d3c5e987bd133ee3b45
crissalave <61393671+crissalave@users.noreply.github.com>
2022-11-14 21:21:37 -0500

    Java Básico: Ejercicio tema 1

---
+++ Java/exercises/topic-1/src/com/exerciseOne/Main.java
@@ -0,0 +1,39 @@
+package com.exerciseOne;
+
+public class Main {
+    public static void main(String[] args) {
+        // 8 bits
+        byte n1 = 3;
+        // 16 bits
+        short n2 = 5;
+        // 32 bits
+        int n3 = 7;
+        // 64 bits
+        long n4 = 11;
+
+        //32 bits
+        float d1 = 13.5f;
+        // 64 bits
+        double d2 = 15.5d;
+
+        // 1 bits
+        boolean b1 = true;
+
+        String txt1 = "Hello";
+        // 16 bits
+        char txt2 = 'c';
+
+        System.out.println("byte: " + n1);
+        System.out.println("short: " + n2);
+        System.out.println("int: " + n3);
+        System.out.println("long: " + n4);
+
+        System.out.println("float: " + d1);
+        System.out.println("double: " + d2);
+
+        System.out.println("boolean: " + b1);
+
+        System.out.println("String: " + txt1);
+        System.out.println("char: " + txt2);
+    }
+}
