cb43934b359eb1a6f3c3122d136838f6e30f177a
crissalave <61393671+crissalave@users.noreply.github.com>
2022-11-17 09:53:05 -0500

    Java Basico: Ejercicio tema 3

---
+++ Java/exercises/topic-3/src/com/exercise3/Concat.java
@@ -0,0 +1,11 @@
+package com.exercise3;
+
+public class Concat {
+    public static void main(String[] args) {
+        String[] txt = {"hola,", "este", "es", "el", "ejercicio", "del", "tema", "3"};
+
+        for (String t : txt) {
+            System.out.print(t + " ");
+        }
+    }
+}
